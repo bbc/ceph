fb8b38f22f5b4ae52ad6fc1f84fa4eada6f76065
James P. Weaver <james.barrett@bbc.co.uk>
2019-01-14 16:09:13 +0000

    rgw: Added caching for tokens retrieved from keystone using S3 credentials

--- make-debs.sh
+++ make-debs.sh
@@ -68,11 +68,12 @@ fi
 # b) do not sign the packages
 # c) use half of the available processors
 #
-: ${NPROC:=$(($(nproc) / 2))}
-if test $NPROC -gt 1 ; then
-    j=-j${NPROC}
-fi
-PATH=/usr/lib/ccache:$PATH dpkg-buildpackage $j -uc -us
+#: ${NPROC:=$(($(nproc) / 2))}
+#if test $NPROC -gt 1 ; then
+#    j=-j${NPROC}
+#fi
+#PATH=/usr/lib/ccache:$PATH
+dpkg-buildpackage $j -uc -us
 cd ../..
 mkdir -p $codename/conf
 cat > $codename/conf/distributions <<EOF
